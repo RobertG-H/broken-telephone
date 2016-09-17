78cf7322bde09b3ec2c5e10188e4a83da239df7b
Jason Hong <r25hong@edu.uwaterloo.ca>
2016-09-17 17:55:09 -0400

    Added input textbox with auto-expanding

--- js/main.js
+++ js/main.js
@@ -1,41 +1,29 @@
-$(function() {
+(function(){
+    var min = 100, max = 900, pad_right = 5, input = document.getElementById('adjinput');
 
-    var newHash      = "",
-        $mainContent = $("#main-content"),
-        $pageWrap    = $("#page-wrap"),
-        baseHeight   = 0
-,        $el;
-        
-    $pageWrap.height($pageWrap.height());
-    baseHeight = $pageWrap.height() - $mainContent.height();
-    
-    $("nav").delegate("a", "click", function() {
-        window.location.hash = $(this).attr("href");
-        return false;
-    });
-        
-    $(window).bind('hashchange', function(){
-    
-        newHash = window.location.hash.substring(1);
-        
-        if (newHash) {
-            $mainContent
-                .find("#guts")
-                .fadeOut(200, function() {
-                    $mainContent.hide().load(newHash + " #guts", function() {
-                        $mainContent.fadeIn(200, function() {
-                            $pageWrap.animate({
-                                height: baseHeight + $mainContent.height() + "px"
-                            });
-                        });
-                        $("nav a").removeClass("current");
-                        $("nav a[href='"+newHash+"']").addClass("current");
-                    });
-                });
-        };
-        
-    });
-    
-    $(window).trigger('hashchange');
-
-});
+    input.style.width = min+'px';
+    input.onkeypress = input.onkeydown = input.onkeyup = function(){
+        var input = this;
+        setTimeout(function(){
+            var tmp = document.createElement('div');
+            tmp.style.padding = '0';
+            if(getComputedStyle)
+                tmp.style.cssText = getComputedStyle(input, null).cssText;
+            if(input.currentStyle)
+                tmp.style.cssText = input.currentStyle.cssText;
+            tmp.style.width = '';
+            tmp.style.position = 'absolute';
+            tmp.innerHTML = input.value.replace(/&/g, "&amp;")
+                                       .replace(/</g, "&lt;")
+                                       .replace(/>/g, "&gt;")
+                                       .replace(/"/g, "&quot;")
+                                       .replace(/'/g, "&#039;")
+                                       .replace(/ /g, '&nbsp;');
+            input.parentNode.appendChild(tmp);
+            var width = tmp.clientWidth+pad_right+1;
+            tmp.parentNode.removeChild(tmp);
+            if(min <= width && width <= max)
+                input.style.width = width+'px';
+        }, 1);
+    }
+})();
